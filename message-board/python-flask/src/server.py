3546bac0a4e2568cbef93636fd05296246144e07
Jake Pelter <jake.pelter@gmail.com>
2024-01-13 18:37:17 -0500

    try with app_user default schema

--- message-board/python-flask/src/server.py
+++ message-board/python-flask/src/server.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
 CORS(app, supports_credentials=True) # NO CORS PROBLEM CUZ NO COOKIES?
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mssql+pyodbc://{environ['SQL_SERVER_USERNAME']}:{environ['SQL_SERVER_PASSWORD']}@{environ['SQL_SERVER_HOST']}/{environ['SQL_SERVER_DB']}?driver=ODBC+Driver+17+for+SQL+Server;schema=messageboard;"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mssql+pyodbc://{environ['SQL_SERVER_USERNAME']}:{environ['SQL_SERVER_PASSWORD']}@{environ['SQL_SERVER_HOST']}/{environ['SQL_SERVER_DB']}?driver=ODBC+Driver+17+for+SQL+Server;"
 db = SQLAlchemy(app)
 db_Base = automap_base()
 
