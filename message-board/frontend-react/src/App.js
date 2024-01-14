472a07c598088726e3a6cdd8e2d0911fe8c94c1c
Jake Pelter <jake.pelter@gmail.com>
2024-01-14 13:58:01 -0500

    sleepy db message

--- message-board/frontend-react/src/App.js
+++ message-board/frontend-react/src/App.js
@@ -6,6 +6,8 @@ import Markdown from 'react-markdown';
 function App() {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
+  const [calledAPI, setCalledAPI] = useState(false);
+  const [badAPI, setBadAPI] = useState(false);
 
   useEffect(() => {
     fetchMessages();
@@ -22,10 +24,13 @@ function App() {
         };
       });
       setMessages(decodedData);
+      setBadAPI(false);
       console.log('Messages fetched:', decodedData);
     } catch (error) {
+      setBadAPI(true);
       console.error('Error fetching messages:', error);
     }
+    setCalledAPI(true);
   };
 
   const postMessage = async () => {
@@ -44,53 +49,59 @@ function App() {
         content: newMessage,
         post_time: data.post_time,
       };
+      setBadAPI(false);
       setMessages([...messages, newMessageData]);
       setNewMessage('');
     } catch (error) {
+      setBadAPI(true);
       console.error('Error posting message:', error);
     }
+    setCalledAPI(true);
   };
   return (
     <div className="App">
-      <div className="MessageBoard">
-        <TextField
-          label="New Message"
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-          multiline
-          maxRows={10}
-          inputProps={{ maxLength: 1000 }}
-          sx={{ margin: "10px", width: "80%", maxWidth: "600px"}}
-        />
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <Button variant="contained" color="primary" onClick={postMessage} sx={{ margin: "10px" }}>
-            Post Message
-          </Button>
+      {calledAPI && badAPI &&
+          <div className="NewMessageArea" style={{ display: "flex", justifyContent: "center" }}>
+            <Paper style={{ color: "red", margin: "10px", width: "80%", maxWidth: "600px" }}>
+              Database might be sleeping. Refresh the page in a few seconds...
+            </Paper>
+          </div>
+      }
+      {calledAPI && !badAPI &&
+        <div className="MessageBoard">
+          <TextField
+                label="New Message"
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+                multiline
+                maxRows={10}
+                inputProps={{ maxLength: 1000 }}
+                sx={{ margin: "10px", width: "80%", maxWidth: "600px"}}
+              />
+          <div className="NewMessageArea" style={{ display: "flex", justifyContent: "center" }}>
+            <Button variant="contained" color="primary" onClick={postMessage} sx={{ margin: "10px" }}>
+              Post Message
+            </Button>
+          </div>
+          {[...messages].reverse().map((message) => (
+            <Paper key={message.id} elevation={3} sx={{ padding: "5px", margin: "10px" }}>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{padding: "1px", margin: "2px"}}>
+                  <strong>{message.id}</strong> 
+                </div>
+                <div style={{padding: "1px", margin: "2px"}}>
+                  <strong>{message.post_time}</strong> 
+                </div>
+              </div>
+              <div style={{ padding: "3px", margin: "6px", textAlign: "left"}}>
+              <Markdown components={{ img: ({ node, ...props }) => ( <img {...props} style={{ maxHeight: "250px", maxWidth:"300px" }} alt="" />),}}>
+                  {message.content}
+                </Markdown>
+              </div>
+            </Paper>
+          ))}
         </div>
-        {[...messages].reverse().map((message) => (
-          <Paper key={message.id} elevation={3} sx={{ padding: "5px", margin: "10px" }}>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <div style={{padding: "1px", margin: "2px"}}>
-                <strong>{message.id}</strong> 
-              </div>
-              <div style={{padding: "1px", margin: "2px"}}>
-                <strong>{message.post_time}</strong> 
-              </div>
-            </div>
-            <div style={{ padding: "3px", margin: "6px", textAlign: "left"}}>
-              <Markdown
-                components={{
-                  img: ({ node, ...props }) => (
-                    <img {...props} style={{ maxHeight: "200px" }} />
-                  ),
-                }}
-              >
-                {message.content}
-              </Markdown>
-            </div>
-          </Paper>
-        ))}
-      </div>
+      }
     </div>
   );
 }
